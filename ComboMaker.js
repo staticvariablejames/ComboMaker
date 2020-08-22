1a402d2225e042a0abc7d2f8833cfd66141de214
Static Variable <staticvariablejames@gmail.com>
2020-08-22 16:28:19 -0500

    Add ComboMaker.js.

---
+++ ComboMaker.js
@@ -0,0 +1,20 @@
+var hasBuildingSpecial = function(spells_cast) {
+    return FortuneCookie.FateChecker(spells_cast, 0, 0.15, false).indexOf("Building Special") !== -1
+        || FortuneCookie.FateChecker(spells_cast, 1, 0.15, false).indexOf("Building Special") !== -1
+        || FortuneCookie.FateChecker(spells_cast, 2, 0.15, false).indexOf("Building Special") !== -1;
+}
+
+var comboSearch = function(target, maxRange = 10000) {
+    let current = Game.Objects['Wizard tower'].minigame.spellsCastTotal - 1;
+    let currentWindow = 0;
+    for(let i = 0; i < target; i++) {
+        currentWindow += hasBuildingSpecial(current++);
+    }
+    for(let i = target; i < maxRange; i++) {
+        currentWindow -= hasBuildingSpecial(current - target);
+        currentWindow += hasBuildingSpecial(current++);
+        if(currentWindow === target)
+            return current - target;
+    }
+    return -1;
+}
